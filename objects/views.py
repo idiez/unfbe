fa8c960de0ff7b4c8c76c6bb9827a3304ad439f4
idiez <izan10saz@gmail.com>
2013-08-01 13:24:10 +0200

    json corrected

--- objects/views.py
+++ objects/views.py
@@ -9,7 +9,7 @@ def restore_view(request, user_req):
 	registered = user_obj.nfcpoint_set.filter(registered=True).values('name','posId','date')	
 	for reg in registered:
 		result = result+simplejson.dumps(reg)+", "
-
+	result= result.replace('user_pic_uri', 'pic_uri')
 	result = result[:-2]+'], "visited":['
 
 	visited = user_obj.nfcpoint_set.filter(registered=False).values('name','posId','date')
@@ -19,8 +19,9 @@ def restore_view(request, user_req):
 
 	friends = user_obj.friends.all().values('user_id','user_name','user_pic_uri') 
 	for fri in friends:
-		result = result+simplejson.dumps(fri)+", "
+		result = result+simplejson.dumps(fri).replace('user','friend')+", "
 	result = result[:-2]+']}'
+
 	return HttpResponse(result, mimetype='application/json')	
 
 
