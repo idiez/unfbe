9b5fad5cf90b2f585ff16c039030f377a1ed6f91
idiez <izan10saz@gmail.com>
2013-08-01 10:24:24 +0200

    restore_user view 6

--- unitenfc/settings.py
+++ unitenfc/settings.py
@@ -101,7 +101,7 @@ MIDDLEWARE_CLASSES = (
     # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
 
-ROOT_URLCONF = 'unitenfc.urls'
+ROOT_URLCONF = 'urls'
 
 # Python dotted path to the WSGI application used by Django's runserver.
 WSGI_APPLICATION = 'unitenfc.wsgi.application'
