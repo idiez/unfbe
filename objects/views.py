01f44824588ff25226cad6b6221f0452da1f69cf
idiez <izan10saz@gmail.com>
2013-08-03 00:47:39 +0200

    list order fix

--- objects/views.py
+++ objects/views.py
@@ -10,13 +10,13 @@ def restore_view(request, user_req):
 	#user_obj = UserInfo.objects.get(user_id=user_req)
 	result = simplejson.dumps(UserInfo.objects.all().filter(user_id=user_req).values('user_name','user_pic_uri')[0])[:-1]+', "registered":['
 
-	registered = user_obj.nfcpoint_set.filter(registered=True).order_by('when').values('name','posId','date')	
+	registered = user_obj.nfcpoint_set.filter(registered=True).order_by('when').reverse().values('name','posId','date')	
 	for reg in registered:
 		result = result+simplejson.dumps(reg)+", "
 	result= result.replace('user_pic_uri', 'pic_uri')
 	result = result[:-2]+'], "visited":['
 
-	visited = user_obj.nfcpoint_set.filter(registered=False).order_by('when').values('name','posId','date')
+	visited = user_obj.nfcpoint_set.filter(registered=False).order_by('when').reverse().values('name','posId','date')
 	for vis in visited:
 		result = result+simplejson.dumps(vis)+", "
 	result = result[:-2]+'], "friends":['
