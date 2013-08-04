97d6f6c6a9355d2b12361c882fbf2207a30f9a8b
idiez <izan10saz@gmail.com>
2013-08-05 00:01:47 +0200

    0 ratings division fixed

--- objects/models.py
+++ objects/models.py
@@ -86,9 +86,9 @@ class Wall(models.Model):
 			mean_rating = cummulative/total
 		result = result+str(mean_rating)+'", "my_rating":"'
 		mine =ratings.filter(user_id=user_req)
-		if len(mine)== 0
+		if len(mine) == 0:
 			my_rating = 0
-		else
+		else:
 			my_rating = mine[0].value
 		result = result+str(my_rating)+'", "entry_list": ['
 		entries = self.entry_set.all().order_by('when').reverse().values('time_stamp','message','author_name')
