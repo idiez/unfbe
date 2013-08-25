54be9bf162ea41935c89dbdc61960d4afa326376
idiez <izan10saz@gmail.com>
2013-08-25 18:27:08 +0200

    deactivation of debug mode

--- unitenfc/settings.py
+++ unitenfc/settings.py
@@ -188,5 +188,5 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
 
-MEDIA_ROOT = os.path.join(PROJECT_PATH, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
